b1a9b09ae14abb0816e25c11aeba4a665827bdac
hamid <hamidmsukardi@gmail.com>
2020-06-24 09:34:57 +0700

    finish first course

--- lib/main.dart
+++ lib/main.dart
@@ -10,7 +10,7 @@ class MyApp extends StatelessWidget {
     return MaterialApp(
       home: Scaffold(
         appBar: AppBar(title: Text("Aplikasi Hello World"),),
-        body: Text("Hello World!"),
+        body: Center(child: Text("Hello World!")),
       ),
     );
   }
